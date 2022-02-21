68b8b62e0b7eddf82acc5f6a38168ae920aa055e
ronronscelestes <julie.plantey@gmail.com>
2022-02-21 15:49:03 +0100

    addon in strapi-design-system

--- packages/strapi-design-system/.storybook/preview.js
+++ packages/strapi-design-system/.storybook/preview.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import { ThemeProvider } from '../src/ThemeProvider';
-import { ThemeProvider } from 'emotion-theming';
+import { ThemeProvider } from '../src/ThemeProvider';
 import { VisuallyHidden } from '../src/VisuallyHidden';
 import { lightTheme } from '../src/themes/lightTheme';
 import { darkTheme } from '../src/themes/darkTheme';
@@ -34,18 +33,3 @@ export const parameters = {
     ],
   },
 };
-
-// export const decorators = [
-//   (Story) => (
-//     <ThemeProvider theme={lightTheme}>
-//       <main>
-//         <VisuallyHidden>
-//           {/* Necessary in order to prevent axe core from providing errors on main / heading */}
-//           <h1>Storybook story</h1>
-//         </VisuallyHidden>
-
-//         <Story />
-//       </main>
-//     </ThemeProvider>
-//   ),
-// ];
