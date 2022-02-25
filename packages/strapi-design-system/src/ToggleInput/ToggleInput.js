00d9a70df0ee870df89036213614500d85b64897
HichamELBSI <elabbassih@gmail.com>
2022-02-25 12:00:21 +0100

    Rename Stack size prop to spacing
    Signed-off-by: HichamELBSI <elabbassih@gmail.com>

--- packages/strapi-design-system/src/ToggleInput/ToggleInput.js
+++ packages/strapi-design-system/src/ToggleInput/ToggleInput.js
@@ -17,7 +17,7 @@ export const ToggleInput = ({ size, error, hint, label, name, labelAction, requi
 
   return (
     <FieldWrapper name={name} hint={hint} error={error} id={generatedId}>
-      <Stack size={1}>
+      <Stack spacing={1}>
         <Flex>
           <FieldLabel required={required} action={labelAction}>
             {label}
